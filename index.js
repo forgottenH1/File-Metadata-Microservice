0a2f454163ac0e3a9f207e2e77fb4018b1a64b5d
forgottenH1 <elarabiayoub1@gmail.com>
2025-12-11 09:54:19 +0100

    added multer and api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+............// starting from here//.............
+const multer = require('multer') //...........added
+const upload = multer() //........... added
 
 var app = express();
 
@@ -10,6 +13,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+//.....................uplaod file using the following api :
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+}) 
 
 
 
